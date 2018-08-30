48fa479ba98eb35bce78dc87ca0be7eaa8bbe40e
Emil <e.yusupovone@yandex.ru>
2018-08-30 17:06:45 +0400

    pack

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     entry: "./src/index.js",
     output:{
         path: path.resolve(__dirname, 'public'), 
-        publicPath: '/public',
+        publicPath: '/',
         filename: "bundle.js"       
     },
 
