da982218b0b3d3804593b056c20e48134f387cff
Emil <e.yusupovone@yandex.ru>
2018-09-02 22:18:38 +0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: "./src/index.js",
     output:{
-        path: path.resolve(__dirname, 'public', 'index.html'), 
+        path: path.resolve(__dirname, 'public'), 
         publicPath: '/public/',
         filename: "bundle.js"       
     },
